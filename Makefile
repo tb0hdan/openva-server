c97dc6cf92d4a7625b84442389f13ee27e01a74d
Bohdan Turkynewych <tb0hdan@gmail.com>
2019-03-06 05:51:08 +0200

    cache

--- Makefile
+++ Makefile
@@ -2,7 +2,6 @@ all: gen build
 
 build:
 	@go build -v -x -ldflags="-s -w" .
-	@strip ./openva-server
 
 gen:
 	@protoc -I api/ api/service.proto --go_out=plugins=grpc:api
--- main.go
+++ main.go
@@ -2,23 +2,38 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"github.com/tb0hdan/openva-server/api"
 	"github.com/tb0hdan/openva-server/tts"
 	"google.golang.org/grpc"
 	"io/ioutil"
 	"log"
 	"net"
+	"os"
+	"path"
+	"strings"
 )
 
 const port = ":50001"
 
 type server struct {
-
 }
 
-func (s *server) TTSStringToMP3(ctx context.Context, request *api.TTSRequest) (reply *api.TTSReply, err error){
-	fileName := tts.Say(request.Text)
-	result, err := ioutil.ReadFile(fileName)
+func (s *server) TTSStringToMP3(ctx context.Context, request *api.TTSRequest) (reply *api.TTSReply, err error) {
+	cacheDir := path.Join("cache", "tts")
+	err = os.MkdirAll(cacheDir, 0755)
+	if err != nil {
+		fmt.Println("Cache dir exists")
+	}
+
+	cachedFile := path.Join(cacheDir, strings.ToLower(strings.Replace(request.Text, " ", "_", -1)+".mp3"))
+	_, err = os.Open(cachedFile)
+	if os.IsNotExist(err) {
+		fname := tts.Say(request.Text)
+		os.Rename(fname, cachedFile)
+	}
+
+	result, err := ioutil.ReadFile(cachedFile)
 	if err != nil {
 		return nil, err
 	}
